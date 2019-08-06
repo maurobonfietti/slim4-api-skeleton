6e8c123620db43f66f1ab8976303ca6568d1933b
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2019-08-06 01:35:22 -0300

    Command to generate CRUD endpoints for an entity.

--- src/Commands/Utils/GenerateCrudEntityCommand.php
+++ src/Commands/Utils/GenerateCrudEntityCommand.php
@@ -22,31 +22,36 @@ class GenerateCrudEntityCommand extends Command
         $this->setName('app:generate:entity:endpoints')
             ->setDescription('Generate CrudEntity Command')
             ->setHelp('This command Generate CrudEntity Command. Version: ' . self::COMMAND_VERSION)
-            ->addArgument('entity', InputArgument::REQUIRED, 'Enter the name for the entity/table, to generate CRUD endpoints.');
+            ->addArgument(
+                'entity',
+                InputArgument::REQUIRED,
+                'Enter the name for the entity/table, to generate CRUD endpoints.'
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('Start!');
+        $output->writeln('Starting!');
 
+        // Get Entity Name.
         $entityName = $input->getArgument('entity');
         $output->writeln('Generate New Entity: ' . $entityName);
         $entityName2 = ucfirst($entityName);
 
+        // Get Entity Fields.
         $db = $this->container->get('db');
         $query = "DESC `$entityName`";
         $statement = $db->prepare($query);
         $statement->execute();
         $fields = $statement->fetchAll();
-//        var_dump($fields);
 
+        // Get Dynamic Params and Fields List.
         $paramList = '';
         $paramList2 = '';
         $paramList3 = '';
         $paramList4 = '';
         $paramList5 = '';
         foreach ($fields as $field) {
-//            var_dump($field['Field']);
             $paramList.= sprintf("`%s`, ", $field['Field']);
             $paramList2.= sprintf(":%s, ", $field['Field']);
             $paramList3.= sprintf('$statement->bindParam(\'%s\', $%s->%s);%s', $field['Field'], $entityName, $field['Field'], PHP_EOL);
@@ -56,25 +61,23 @@ class GenerateCrudEntityCommand extends Command
                 $paramList5.= sprintf("if (isset(\$data->%s)) { $%s->%s = \$data->%s; }%s", $field['Field'], $entityName, $field['Field'], $field['Field'], PHP_EOL);
                 $paramList5.= sprintf("%'\t1s", '');
             }
-//            exit;
         }
         $fieldList = substr_replace($paramList, '', -2);
         $fieldList2 = substr_replace($paramList2, '', -2);
-//        $fieldList3 = $paramList3;
         $fieldList3 = substr_replace($paramList3, '', -2);
         $fieldList4 = substr_replace($paramList4, '', -2);
         $fieldList5 = substr_replace($paramList5, '', -2);
-//        $fieldList5 = $paramList5;
-//        var_dump($fieldList5); exit;
 
-        $myquery = '$query = \'INSERT INTO `'.$entityName.'` ('.$fieldList.') VALUES ('.$fieldList2.')\';
+        // Get Base Query For Insert Function.
+        $insertQueryFunction = '$query = \'INSERT INTO `'.$entityName.'` ('.$fieldList.') VALUES ('.$fieldList2.')\';
         $statement = $this->getDb()->prepare($query);
         '.$fieldList3.'
         $statement->execute();
 
         return $this->checkAndGet'.$entityName2.'((int) $this->getDb()->lastInsertId());';
 
-        $myUpdatequery = ''.$fieldList5.'
+        // Get Base Query For Update Function.
+        $updatequeryFunction = ''.$fieldList5.'
 
         $query = \'UPDATE `'.$entityName.'` SET '.$fieldList4.' WHERE `id` = :id\';
         $statement = $this->getDb()->prepare($query);
@@ -83,19 +86,8 @@ class GenerateCrudEntityCommand extends Command
 
         return $this->checkAndGet'.$entityName2.'((int) $'.$entityName.'->id);';
 
-//        var_dump($myUpdatequery); exit;
-
-        /*
-        $query = 'UPDATE `objectbase` SET `objectbase` = :objectbase WHERE `id` = :id';
-        $statement = $this->getDb()->prepare($query);
-        $statement->bindParam('id', $objectbase->id);
-        $statement->bindParam('objectbase', $objectbase->objectbase);
-        $statement->execute();
-
-        return $this->checkAndGetObjectbase((int) $objectbase->id);
-         *          */
-
-        $asd = '
+        // Add Routes.
+        $routes = '
 $app->group("/'.$entityName.'", function () use ($app) {
     $app->get("", "App\Controller\\'.ucfirst($entityName).'\GetAll");
     $app->get("/[{id}]", "App\Controller\\'.ucfirst($entityName).'\GetOne");
@@ -104,95 +96,91 @@ $app->group("/'.$entityName.'", function () use ($app) {
     $app->delete("/[{id}]", "App\Controller\\'.ucfirst($entityName).'\Delete");
 });
 ';
-        $fichero = __DIR__ . '/../../App/Routes.php';
-        $actual = file_get_contents($fichero);
-        $actual.= $asd;
-        file_put_contents($fichero, $actual);
+        $file = __DIR__ . '/../../App/Routes.php';
+        $content = file_get_contents($file);
+        $content.= $routes;
+        file_put_contents($file, $content);
 
-        $asd = '
+        // Add Repositories.
+        $repository = '
 $container["'.$entityName.'_repository"] = function (ContainerInterface $container): App\Repository\\'.ucfirst($entityName).'Repository {
     return new App\Repository\\'.ucfirst($entityName).'Repository($container->get("db"));
 };
 ';
-        $fichero = __DIR__ . '/../../App/Repositories.php';
-        $actual = file_get_contents($fichero);
-        $actual.= $asd;
-        file_put_contents($fichero, $actual);
+        $file = __DIR__ . '/../../App/Repositories.php';
+        $repositoryContent = file_get_contents($file);
+        $repositoryContent.= $repository;
+        file_put_contents($file, $repositoryContent);
 
-        $asd = '
+        // Add Services.
+        $service = '
 $container["'.$entityName.'_service"] = function (ContainerInterface $container): App\Service\\'.ucfirst($entityName).'Service {
     return new App\Service\\'.ucfirst($entityName).'Service($container->get("'.$entityName.'_repository"));
 };
 ';
-        $fichero = __DIR__ . '/../../App/Services.php';
-        $actual = file_get_contents($fichero);
-        $actual.= $asd;
-        file_put_contents($fichero, $actual);
+        $file = __DIR__ . '/../../App/Services.php';
+        $serviceContent = file_get_contents($file);
+        $serviceContent.= $service;
+        file_put_contents($file, $serviceContent);
 
-        /********/
+        // Copy CRUD Template.
+        $source = __DIR__ . '/../../Commands/SourceCode/Objectbase';
+        $target = __DIR__ . '/../../Controller/' . ucfirst($entityName);
+        shell_exec("cp -r $source $target");
 
-        $origen = __DIR__ . '/../../Commands/SourceCode/Objectbase';
-        $destino = __DIR__ . '/../../Controller/' . ucfirst($entityName);
-        shell_exec("cp -r $origen $destino");
-
-//        $entityName2 = ucfirst($entityName);
-        $base = $destino . '/Base.php';
+        // Replace CRUD Controller Template for New Entity.
+        $base = $target . '/Base.php';
         shell_exec("sed -i .bkp -e 's/Objectbase/$entityName2/g' $base");
         shell_exec("sed -i .bkp -e 's/objectbase/$entityName/g' $base");
+        shell_exec("sed -i .bkp -e 's/Objectbase/$entityName2/g' $target/Create.php");
+        shell_exec("sed -i .bkp -e 's/objectbase/$entityName/g' $target/Create.php");
+        shell_exec("sed -i .bkp -e 's/Objectbase/$entityName2/g' $target/Delete.php");
+        shell_exec("sed -i .bkp -e 's/objectbase/$entityName/g' $target/Delete.php");
+        shell_exec("sed -i .bkp -e 's/Objectbase/$entityName2/g' $target/GetAll.php");
+        shell_exec("sed -i .bkp -e 's/objectbase/$entityName/g' $target/GetAll.php");
+        shell_exec("sed -i .bkp -e 's/Objectbase/$entityName2/g' $target/GetOne.php");
+        shell_exec("sed -i .bkp -e 's/objectbase/$entityName/g' $target/GetOne.php");
+        shell_exec("sed -i .bkp -e 's/Objectbase/$entityName2/g' $target/Update.php");
+        shell_exec("sed -i .bkp -e 's/objectbase/$entityName/g' $target/Update.php");
 
-        shell_exec("sed -i .bkp -e 's/Objectbase/$entityName2/g' $destino/Create.php");
-        shell_exec("sed -i .bkp -e 's/objectbase/$entityName/g' $destino/Create.php");
+        // Remove Any Temp Files.
+        shell_exec("rm -f $target/*.bkp");
 
-        shell_exec("sed -i .bkp -e 's/Objectbase/$entityName2/g' $destino/Delete.php");
-        shell_exec("sed -i .bkp -e 's/objectbase/$entityName/g' $destino/Delete.php");
+        // Replace and Update Exceptions
+        $source = __DIR__ . '/../../Commands/SourceCode/ObjectbaseException.php';
+        $target = __DIR__ . '/../../Exception/' . ucfirst($entityName). 'Exception.php';
+        shell_exec("cp $source $target");
+        shell_exec("sed -i .bkp -e 's/Objectbase/$entityName2/g' $target");
+        shell_exec("sed -i .bkp -e 's/objectbase/$entityName/g' $target");
+        shell_exec("rm -f $target.bkp");
 
-        shell_exec("sed -i .bkp -e 's/Objectbase/$entityName2/g' $destino/GetAll.php");
-        shell_exec("sed -i .bkp -e 's/objectbase/$entityName/g' $destino/GetAll.php");
+        // Replace and Update Services.
+        $source = __DIR__ . '/../../Commands/SourceCode/ObjectbaseService.php';
+        $target = __DIR__ . '/../../Service/' . ucfirst($entityName). 'Service.php';
+        shell_exec("cp $source $target");
+        shell_exec("sed -i .bkp -e 's/Objectbase/$entityName2/g' $target");
+        shell_exec("sed -i .bkp -e 's/objectbase/$entityName/g' $target");
+        shell_exec("rm -f $target.bkp");
 
-        shell_exec("sed -i .bkp -e 's/Objectbase/$entityName2/g' $destino/GetOne.php");
-        shell_exec("sed -i .bkp -e 's/objectbase/$entityName/g' $destino/GetOne.php");
-
-        shell_exec("sed -i .bkp -e 's/Objectbase/$entityName2/g' $destino/Update.php");
-        shell_exec("sed -i .bkp -e 's/objectbase/$entityName/g' $destino/Update.php");
-
-        shell_exec("rm -f $destino/*.bkp");
-
-        $origen = __DIR__ . '/../../Commands/SourceCode/ObjectbaseException.php';
-        $destino = __DIR__ . '/../../Exception/' . ucfirst($entityName). 'Exception.php';
-        shell_exec("cp $origen $destino");
-        shell_exec("sed -i .bkp -e 's/Objectbase/$entityName2/g' $destino");
-        shell_exec("sed -i .bkp -e 's/objectbase/$entityName/g' $destino");
-        shell_exec("rm -f $destino.bkp");
-
-        $origen = __DIR__ . '/../../Commands/SourceCode/ObjectbaseService.php';
-        $destino = __DIR__ . '/../../Service/' . ucfirst($entityName). 'Service.php';
-        shell_exec("cp $origen $destino");
-        shell_exec("sed -i .bkp -e 's/Objectbase/$entityName2/g' $destino");
-        shell_exec("sed -i .bkp -e 's/objectbase/$entityName/g' $destino");
-        shell_exec("rm -f $destino.bkp");
-
-        $origen = __DIR__ . '/../../Commands/SourceCode/ObjectbaseRepository.php';
-        $destino = __DIR__ . '/../../Repository/' . ucfirst($entityName). 'Repository.php';
-        shell_exec("cp $origen $destino");
-        shell_exec("sed -i .bkp -e 's/Objectbase/$entityName2/g' $destino");
-        shell_exec("sed -i .bkp -e 's/objectbase/$entityName/g' $destino");
+        // Replace and Update Repository.
+        $source = __DIR__ . '/../../Commands/SourceCode/ObjectbaseRepository.php';
+        $target = __DIR__ . '/../../Repository/' . ucfirst($entityName). 'Repository.php';
+        shell_exec("cp $source $target");
+        shell_exec("sed -i .bkp -e 's/Objectbase/$entityName2/g' $target");
+        shell_exec("sed -i .bkp -e 's/objectbase/$entityName/g' $target");
 //        shell_exec("sed -i .bkp -e 's/#ppp/$myquery/g' $destino");
-        shell_exec("rm -f $destino.bkp");
+        shell_exec("rm -f $target.bkp");
 
-        $fichero = $destino;
-        $actual = file_get_contents($fichero);
-//        $actual.= $myquery;
-        $myoutput = preg_replace("/".'#ppp'."/", $myquery, $actual);
-//        var_dump($output); exit;
-        file_put_contents($fichero, $myoutput);
+        // Replace and Update Repository Insert Function.
+        $entityRepository = file_get_contents($target);
+        $repositoryData = preg_replace("/".'#ppp'."/", $insertQueryFunction, $entityRepository);
+        file_put_contents($target, $repositoryData);
 
-        $fichero = $destino;
-        $actual = file_get_contents($fichero);
-//        $actual.= $myquery;
-        $myoutput = preg_replace("/".'#uuu'."/", $myUpdatequery, $actual);
-//        var_dump($output); exit;
-        file_put_contents($fichero, $myoutput);
+        // Replace and Update Repository Update Function.
+        $entityRepositoryUpdate = file_get_contents($target);
+        $repositoryDataUpdate = preg_replace("/".'#uuu'."/", $updatequeryFunction, $entityRepositoryUpdate);
+        file_put_contents($target, $repositoryDataUpdate);
 
-        $output->writeln('End ;-)');
+        $output->writeln('Script Finish ;-)');
     }
 }
