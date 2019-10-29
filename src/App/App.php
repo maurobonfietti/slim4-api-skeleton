e83e247fa0e97a98075fec9c2e1030b38a67b93b
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2019-10-28 23:22:08 -0300

    Allow CORS in Slim 4.

--- src/App/App.php
+++ src/App/App.php
@@ -38,7 +38,22 @@ $customErrorHandler = function (ServerRequestInterface $request, Throwable $exce
 $errorMiddleware = $app->addErrorMiddleware(true, true, true);
 $errorMiddleware->setDefaultErrorHandler($customErrorHandler);
 
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+$app->add(function ($request, $handler) {
+    $response = $handler->handle($request);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
 require __DIR__ . '/Dependencies.php';
 require __DIR__ . '/Services.php';
 require __DIR__ . '/Repositories.php';
 require __DIR__ . '/Routes.php';
+
+$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function ($request, $response) {
+    throw new HttpNotFoundException($request);
+});
