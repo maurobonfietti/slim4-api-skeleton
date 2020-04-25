456111269cfd4883abb216e0aac05ab760707829
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2020-04-25 13:23:34 -0300

    Fix some issues detected with PHP Insights.

--- src/Controller/Home.php
+++ src/Controller/Home.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Pimple\Psr11\Container;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 final class Home
 {
@@ -14,17 +14,14 @@ final class Home
 
     private const API_VERSION = '0.5.0';
 
-    /**
-     * @var Container 
-     */
-    private $container;
+    private Container $container;
 
     public function __construct(Container $container)
     {
         $this->container = $container;
     }
 
-    public function getHelp(Request $request, Response $response)
+    public function getHelp(Request $request, Response $response): Response
     {
         $message = [
             'api' => self::API_NAME,
@@ -37,7 +34,7 @@ final class Home
         return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
     }
 
-    public function getStatus(Request $request, Response $response)
+    public function getStatus(Request $request, Response $response): Response
     {
         $this->container->get('db');
         $status = [
