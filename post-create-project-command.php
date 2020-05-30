e3c686feaa9efc87c3f137d53f3826381f2cde34
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2020-05-30 16:10:29 -0300

    Minor changes for post-create-project-cmd.

--- post-create-project-command.php
+++ post-create-project-command.php
@@ -4,20 +4,32 @@ declare(strict_types=1);
 
 final class PostCreateProjectCommand
 {
+    public static function showIntro(): void
+    {
+        echo <<<EOF
+   _____ _ _             _  _           
+  / ____| (_)           | || |          
+ | (___ | |_ _ __ ___   | || |_         
+  \___ \| | | '_ ` _ \  |__   _|        
+  ____) | | | | | | | |    | |          
+ |_____/|_|_|_| |_| |_|    |_|       _  
+ |  __ \         | |     /\         (_) 
+ | |__) |___  ___| |_   /  \   _ __  _  
+ |  _  // _ \/ __| __| / /\ \ | '_ \| | 
+ | | \ \  __/\__ \ |_ / ____ \| |_) | | 
+ |_|__\_\___||___/\__/_/ _  \_\ .__/|_| 
+  / ____| |      | |    | |   | |       
+ | (___ | | _____| | ___| |_ _|_| _ __  
+  \___ \| |/ / _ \ |/ _ \ __/ _ \| '_ \ 
+  ____) |   <  __/ |  __/ || (_) | | | |
+ |_____/|_|\_\___|_|\___|\__\___/|_| |_|
+
+EOF;
+    }
+
     public static function showMessage(): void
     {
         echo <<<EOF
-   _____ _ _             _  _              
-  / ____| (_)           | || |             
- | (___ | |_ _ __ ___   | || |_            
-  \___ \| | | '_ ` _ \  |__   _|           
-  ____) | | | | | | | |    | |             
- |_____/|_|_|_| |_| |_|    |_| _____ _____ 
-  / ____| |      | |     /\   |  __ \_   _|
- | (___ | | _____| |    /  \  | |__) || |  
-  \___ \| |/ / _ \ |   / /\ \ |  ___/ | |  
-  ____) |   <  __/ |  / ____ \| |    _| |_ 
- |_____/|_|\_\___|_| /_/    \_\_|   |_____|
 
 *************************************************************
 Project: https://github.com/maurobonfietti/slim4-api-skeleton
@@ -27,11 +39,11 @@ Successfully created project!
 
 Get started with the following commands:
 
-$ cd [my-api-name]
+$ cd [your-api-name]
 $ composer test
 $ composer start
 
-(P.S. set your MySQL connection in .env file)
+(P.S. set your MySQL connection in '.env' file)
 
 Thanks for installing this project!
 
@@ -41,4 +53,5 @@ EOF;
     }
 }
 
+PostCreateProjectCommand::showIntro();
 PostCreateProjectCommand::showMessage();
