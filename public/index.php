e91336a24df386bd6f73d8d7b347ffa3ad4f209c
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2020-06-09 01:04:47 -0300

    Minor changes.

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 require __DIR__ . '/../src/App/App.php';
+
 $app = new App();
-$app = $app->getAppInstance();
-$app->run();
+$application = $app->getAppInstance();
+$application->run();
