8705e2b15b6b9ab4e93109d69159f13958531c17
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2020-06-09 00:58:11 -0300

    Remove Settings.php

--- src/App/Dependencies.php
+++ src/App/Dependencies.php
@@ -2,12 +2,9 @@
 
 declare(strict_types=1);
 
-use Pimple\Container;
-
-$container['db'] = static function (Container $c): PDO {
-    $db = $c['settings']['db'];
-    $database = sprintf('mysql:host=%s;dbname=%s', $db['hostname'], $db['database']);
-    $pdo = new PDO($database, $db['username'], $db['password']);
+$container['db'] = static function (): PDO {
+    $db = sprintf('mysql:host=%s;dbname=%s', getenv('DB_HOST'), getenv('DB_NAME'));
+    $pdo = new PDO($db, getenv('DB_USER'), getenv('DB_PASS'));
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
