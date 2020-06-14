9bfdd8bdb61ff892a92852a0c7b4d5af8831d104
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2020-06-14 20:24:19 -0300

    Rollback change.

--- src/App/App.php
+++ src/App/App.php
@@ -8,7 +8,7 @@ use Slim\Factory\AppFactory;
 
 final class App
 {
-    public function getAppInstance(): AppFactory
+    public function getAppInstance()
     {
         require __DIR__ . '/../../vendor/autoload.php';
 
