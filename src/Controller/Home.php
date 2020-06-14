52dc9534659d9c9334513b8333a2999398880ff8
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2020-06-13 23:18:45 -0300

    Update API version.

--- src/Controller/Home.php
+++ src/Controller/Home.php
@@ -13,7 +13,7 @@ final class Home
 {
     private const API_NAME = 'slim4-api-skeleton';
 
-    private const API_VERSION = '0.12.0';
+    private const API_VERSION = '0.13.0';
 
     private Container $container;
 
