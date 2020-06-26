e0ff34ac71fd42c21e4fed2f750c1ae1f306a8d8
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2020-06-26 19:41:59 -0300

    Move JsonResponse to Helper.

--- src/Controller/Home.php
+++ src/Controller/Home.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Lib\JsonResponse;
+use App\Helper\JsonResponse;
 use Pimple\Psr11\Container;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
