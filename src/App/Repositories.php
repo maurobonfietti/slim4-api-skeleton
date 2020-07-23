18866a46d5c08a5c879129e3f8b1aedfece3495c
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2020-07-23 00:08:23 -0300

    Cleaning.

--- src/App/Repositories.php
+++ src/App/Repositories.php
@@ -1,7 +1,3 @@
 <?php
 
 declare(strict_types=1);
-
-$container["restaurantes_repository"] = static function ($container): App\Repository\RestaurantesRepository {
-    return new App\Repository\RestaurantesRepository($container["db"]);
-};
--- src/App/Services.php
+++ src/App/Services.php
@@ -1,7 +1,3 @@
 <?php
 
 declare(strict_types=1);
-
-$container["restaurantes_service"] = static function ($container): App\Service\RestaurantesService {
-    return new App\Service\RestaurantesService($container["restaurantes_repository"]);
-};
