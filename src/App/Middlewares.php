18f94b7e5edadcd237da3025eb28b997be08d290
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2020-08-29 23:49:03 -0300

    Minor changes.

--- src/App/Middlewares.php
+++ src/App/Middlewares.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-$path = isset($_SERVER['SLIM_BASE_PATH']) ? $_SERVER['SLIM_BASE_PATH'] : '';
+$path = $_SERVER['SLIM_BASE_PATH'] ?? '';
 $app->setBasePath($path);
 $app->addRoutingMiddleware();
 $app->addBodyParsingMiddleware();
 $displayError = filter_var(
-    isset($_SERVER['DISPLAY_ERROR_DETAILS']) ? $_SERVER['DISPLAY_ERROR_DETAILS'] : false,
+    $_SERVER['DISPLAY_ERROR_DETAILS'] ?? false,
     FILTER_VALIDATE_BOOLEAN
 );
 $errorMiddleware = $app->addErrorMiddleware($displayError, true, true);
