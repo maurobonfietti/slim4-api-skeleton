781798a949d1310197c5ec7fff3119c2dae5cb13
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2020-09-06 15:16:18 -0300

    Create Dotenv in a safe way.

--- src/App/DotEnv.php
+++ src/App/DotEnv.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 $baseDir = __DIR__ . '/../../';
-$dotenv = Dotenv\Dotenv::createUnsafeImmutable($baseDir);
+$dotenv = Dotenv\Dotenv::createImmutable($baseDir);
 if (file_exists($baseDir . '.env')) {
     $dotenv->load();
 }
