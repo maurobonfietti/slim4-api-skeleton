c1994069cd5fc4f8ceff9050b5a6064cf08d21b5
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2020-09-20 01:54:35 -0300

    Minor changes.

--- src/App/ErrorHandler.php
+++ src/App/ErrorHandler.php
@@ -2,8 +2,8 @@
 
 declare(strict_types=1);
 
-use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface;
 
 $customErrorHandler = function (
     ServerRequestInterface $request,
