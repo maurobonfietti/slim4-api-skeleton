dbca3d78e9039d39be4efb4d2ac941eeb5b06295
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2020-11-14 23:13:17 -0300

    Refactor src/App.

--- src/App/App.php
+++ src/App/App.php
@@ -8,10 +8,10 @@ $app = require __DIR__ . '/Container.php';
 $customErrorHandler = require __DIR__ . '/ErrorHandler.php';
 (require __DIR__ . '/Middlewares.php')($app, $customErrorHandler);
 (require __DIR__ . '/Cors.php')($app);
-require __DIR__ . '/Database.php';
-require __DIR__ . '/Services.php';
-require __DIR__ . '/Repositories.php';
-require __DIR__ . '/Routes.php';
+(require __DIR__ . '/Database.php');
+(require __DIR__ . '/Services.php');
+(require __DIR__ . '/Repositories.php');
+(require __DIR__ . '/Routes.php');
 (require __DIR__ . '/NotFound.php')($app);
 
 return $app;
